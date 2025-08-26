ffd6ac079297b7c24f4d7e3530ab67cabd64c95c
ApenasAlguem007 <geovanna.psilva2005@gmail.com>
2025-08-26 12:06:00 -0300

    26.08

---
+++ PictureBox/PictureBox.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBox
+{
+    public partial class PictureBox : Form
+    {
+        private string imagemLocalizada;
+      
+
+        public PictureBox()
+        {
+           
+            InitializeComponent();
+        }
+
+        private void btnVerImagem_Click(object sender, EventArgs e)
+        {
+            pbCidade.Image = Image.FromFile(@"C:\Users\geovanna.psilva9\Downloads\Sampa.PNG");
+            pbCidade.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btnAnexar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // caixa de dialogo para abrir o arquivo
+                OpenFileDialog abrirarquivo = new OpenFileDialog();
+                abrirarquivo.Filter = "jpg files (*.jpg)|*.jpg|PNG file(*.png)|*.png|All files (*.*)|*.*";
+
+                if (abrirarquivo.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imagemLocalizada = abrirarquivo.FileName;
+                    pbAnexarImagem.ImageLocation = imagemLocalizada;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocorreu um erro", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
+
+
---
+++ crud/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace crud
+{
+    public partial class frmCadastroDeClientes : Form
+    {
+
+        // Conexão com o banco de dados MySql
+        MySqlConnection Conexao;
+        string data_source = "datasource=localhost; username=root; password=; database=db_cadastro";
+
+        public frmCadastroDeClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validação de Campos Obrigatorios
+
+                if(string.IsNullOrEmpty(txtNomeCompleto.Text.Trim()) ||
+                    string.IsNullOrEmpty(txtEmail.Text.Trim()) ||
+                    string.IsNullOrEmpty(txtCPF.Text.Trim()))
+                {
+                    MessageBox.Show("Todos os campos devem ser preenchidos.",
+                        "Validação",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning
+                        );
+                    return; //impede o prosseguimento se algum campo estiver vazio
+                }
+
+                // validação de CPF
+
+                string cpf = txtCPF.Text.Trim();
+
+                if (!IsValidCPFLength(cpf))
+                {
+                    MessageBox.Show("CPF inválido Certifique-se de que o CPF tenha 11 digitos númericos.",
+                        "Validação",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning);
+                    return; // Impede o prosseguimento se o CPF for válido
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        // Função para válidar o comprimento e formato do CPF
+        private bool IsValidCPFLength(string cpf)
+        {
+
+            //Remove todos os caracteres não numericos
+            cpf = new string(cpf.Where(char.IsDigit).ToArray());
+
+            // Verifica se o CPF tem exatamente 11 digitos
+            return cpf.Length == 11;
+
+        }
+    }
+}
